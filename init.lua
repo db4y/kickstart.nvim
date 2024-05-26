9e9d937c37c003e16002710674ce8a2c9f76a3a1
dbessey <daniel.bessey@unistra.fr>
2024-05-26 09:31:03 +0200

    Don't autoformat python on save, keep upstream config comments

--- init.lua
+++ init.lua
@@ -648,7 +648,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, python = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -657,16 +657,16 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        -- javascript = { { "prettierd", "prettier" } },     formatters_by_ft = {
         css = { 'prettierd', 'prettier' },
         javascript = { 'prettierd', 'prettier' },
         json = { 'prettierd', 'prettier' },
         markdown = { 'prettierd', 'prettier' },
         python = { 'black', 'isort' },
-
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
       },
     },
   },
