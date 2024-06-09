2b104289f241688c045a40f6ae44f067c2593692
dbessey <daniel.bessey@unistra.fr>
2024-06-09 08:41:20 +0200

    🧹 Clean-up

--- init.lua
+++ init.lua
@@ -688,7 +688,7 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },     formatters_by_ft = {
+        -- javascript = { { "prettierd", "prettier" } },
         css = { 'prettierd', 'prettier' },
         javascript = { 'prettierd', 'prettier' },
         json = { 'jq' },
